ad05799ea3ddde8443e5f001ab67fd0e56c3360a
stonefullstm <stonefull.stm@gmail.com>
2025-08-28 19:23:51 -0300

    feat: Cria componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home.component')
+    .then(mod => mod.HomeComponent)
+  },
+];
